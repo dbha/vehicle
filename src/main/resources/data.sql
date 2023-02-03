544e7b7f3e7d8948650f8e86b140795df1b609c5
dbha <dbha0719@gmail.com>
2023-02-03 14:41:52 +0900

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,51 @@ INSERT INTO vehicle(name) VALUES ('Yaris') ON CONFLICT ON CONSTRAINT vehicle_nam
 INSERT INTO vehicle(name) VALUES ('Vios') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
 INSERT INTO vehicle(name) VALUES ('Glanza') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
 INSERT INTO vehicle(name) VALUES ('Aygo') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+
+INSERT INTO vehicle(name) VALUES ('Avalon1') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Corolla1') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Crown1') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Levin1') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Yaris1') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Vios1') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Glanza1') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Aygo1') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+
+INSERT INTO vehicle(name) VALUES ('Avalon2') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Corolla2') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Crown2') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Levin2') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Yaris2') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Vios2') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Glanza2') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Aygo2') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+
+INSERT INTO vehicle(name) VALUES ('Avalon3') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Corolla3') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Crown3') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Levin3') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Yaris3') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Vios3') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Glanza3') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Aygo3') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+
+INSERT INTO vehicle(name) VALUES ('Avalon4') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Corolla4') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Crown4') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Levin4') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Yaris4') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Vios4') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Glanza4') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Aygo4') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+
+
+INSERT INTO vehicle(name) VALUES ('Avalon5') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Corolla5') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Crown5') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Levin5') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Yaris5') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Vios5') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Glanza5') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+INSERT INTO vehicle(name) VALUES ('Aygo5') ON CONFLICT ON CONSTRAINT vehicle_name_key DO NOTHING;
+
+
